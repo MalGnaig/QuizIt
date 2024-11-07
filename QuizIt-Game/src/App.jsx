6b779d1765b6b5556e83a66f76a2ba9d59e329e5
Lam Giang <93488338+MalGnaig@users.noreply.github.com>
2024-11-07 16:59:52 +0000

    App created

---
+++ QuizIt-Game/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-slate-50'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
